95f74dfe24dc2c7b46c17b855d7db7d6ed71bd32
Anderson <ladoleste1@gmail.com>
2017-12-12 09:01:34 -0200

    unit tests

--- app/src/main/java/com/thevacationplanner/app/Util.kt
+++ app/src/main/java/com/thevacationplanner/app/Util.kt
@@ -1,8 +1,14 @@
 package com.thevacationplanner.app
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
  *Created by Anderson on 11/12/2017.
  */
-fun String.capitalizeWords(): String {
-    return this.split(" ").joinToString(" ") { it.capitalize() }
-}
+fun String.capitalizeWords(): String = split(" ").joinToString(" ") { it.capitalize() }
+
+fun Date.asString(format: DateFormat): String = format.format(this)
+
+fun Date.asString(format: String): String = asString(SimpleDateFormat(format, Locale.US))
--- app/src/test/java/com/thevacationplanner/ExampleUnitTest.kt
+++ app/src/test/java/com/thevacationplanner/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.thevacationplanner
 
+import com.thevacationplanner.app.asString
 import com.thevacationplanner.app.capitalizeWords
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,10 @@ class ExampleUnitTest {
     @Test
     fun capitalizeWords() {
         assertEquals("The Vacation Planner", "the vacation planner".capitalizeWords())
+    }
+
+    @Test
+    fun dateAsString() {
+        assertEquals("Wed, Dec 31", Date(3443343).asString("EEE, MMM d"))
     }
 }
